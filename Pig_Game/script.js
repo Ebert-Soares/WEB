0c6f7ab8f5b58aafe8977dcfdf6001115e881a89
Ebert Soares <59069184+Ebert-Soares@users.noreply.github.com>
2021-01-28 12:53:31 -0300

    Update script.js

--- Pig_Game/script.js
+++ Pig_Game/script.js
@@ -10,7 +10,6 @@ activePlayer = 0;
 
 
 document.querySelector('.dice').style.display = 'none';
-//document.querySelector('#current--' + activePlayer).textContent = dice ;
 document.getElementById('score--0').textContent = scores[0];
 document.getElementById('score--1').textContent = scores[1];
 document.getElementById('current--0').textContent = '0';
