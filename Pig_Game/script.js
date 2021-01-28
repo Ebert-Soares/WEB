e3ae672b3a1eeadce4152728212497ac915456d5
Ebert Soares <59069184+Ebert-Soares@users.noreply.github.com>
2021-01-28 12:46:40 -0300

    Update script.js

--- Pig_Game/script.js
+++ Pig_Game/script.js
@@ -1,4 +1,5 @@
 
+
 var scores, roundScore, activePlayer;
 var dice;
 
@@ -44,10 +45,19 @@ if(dice > 1){
 document.querySelector('.btn--hold').addEventListener('click', function(){
   scores[activePlayer] += roundScore;
   document.getElementById('score--' + activePlayer).textContent = scores[activePlayer];
+    if(scores[activePlayer] >= 100){
+      document.title = 'Player ' + activePlayer + ' won';
+      alert('Player ' + activePlayer + ' won');
+      document.querySelector('.dice').style.display = 'none';
+      //document.querySelector('#current--' + activePlayer).textContent = dice ;
+      location.reload();
+
+  }
   document.getElementById('current--' + activePlayer).textContent = 0;
   document.querySelector('.player--' + activePlayer).classList.remove('player--active');
   activePlayer === 0? activePlayer = 1 : activePlayer = 0;
   roundScore = 0;
   document.querySelector('.player--' + activePlayer).classList.add('player--active');
   document.querySelector('.dice').style.display = 'none';
+
 });
