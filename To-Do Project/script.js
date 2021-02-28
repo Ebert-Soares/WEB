833335c70a53a90c7b32791f16e393556bb4d456
Ebert Soares <59069184+Ebert-Soares@users.noreply.github.com>
2021-02-28 19:58:01 -0300

    Created boxes and started js

---
+++ To-Do Project/script.js
@@ -0,0 +1,11 @@
+
+var task;
+
+function task(){
+var task = prompt("Digite a sua task: ");
+
+
+}
+//todo: create the "turn white" system
+//todo: make the getelement and change the text inside it
+//todo: make the divs draggable
