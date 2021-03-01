b73279ab757b26367e46d4994be918986ba28271
Ebert Soares <59069184+Ebert-Soares@users.noreply.github.com>
2021-03-01 15:34:52 -0300

    Changed some things, not completed

--- To-Do Project/script.js
+++ To-Do Project/script.js
@@ -1,19 +1,20 @@
 
 var task;
 var retangleNumber = 0;
+var rewrite;
+var newtask;
 
-
-function task(){
+function createTask(){
 retangleNumber+=1;
-var task = prompt("Digite a sua task: (maximum of 36 characters)");
+var task = prompt("Digite a sua task: (maximum of 60 characters)");
 
-if(task.length<36 && task.length>12){
+if(task.length<60 && task.length>20){
 document.getElementById('retangle' + retangleNumber).style.backgroundColor = 'white';
 document.getElementById('retangle' + retangleNumber).style.border = '2px solid grey';
-document.getElementById('retangle' + retangleNumber).style.height =  1 + Math.round(task.length/12) + 'rem';
+document.getElementById('retangle' + retangleNumber).style.height =  1 + Math.round(task.length/20) + 'rem';
 document.getElementById('retangle' + retangleNumber).textContent = ' ' + task ;}
 
-else if(task.length<12 && task.length>0){
+else if(task.length<20 && task.length>0){
     document.getElementById('retangle' + retangleNumber).style.backgroundColor = 'white';
     document.getElementById('retangle' + retangleNumber).style.height =   2 +'rem';
     document.getElementById('retangle' + retangleNumber).textContent = ' ' + task
@@ -22,9 +23,33 @@ else if(task.length<12 && task.length>0){
 }
 
 else{
-  alert('More than 36 characters')
+  alert('More than 60 characters');
 }
 }
+
+
+function doubleclick(rewrite){
+
+var newtask = prompt("Digite a sua task: (maximum of 60 characters)");
+
+if(newtask.length<60 && newtask.length>20){
+document.getElementById('retangle' + rewrite).style.height =  1 + Math.round(newtask.length/20) + 'rem';
+document.getElementById('retangle' + rewrite).textContent = ' ' + newtask ;}
+
+else if(task.length<20 && task.length>0){
+
+    document.getElementById('retangle' + retangleNumber).style.height =   2 +'rem';
+    document.getElementById('retangle' + retangleNumber).textContent = ' ' + task;
+
+}}
+
+
+document.getElementById("retangle1").addEventListener("dblclick", doubleclick(rewrite=1));
+document.getElementById("retangle2").addEventListener("dblclick", doubleclick(rewrite=2));
+document.getElementById("retangle3").addEventListener("dblclick", doubleclick(rewrite=3));
+document.getElementById("retangle1").addEventListener("dblclick", doubleclick(rewrite=4));
+
 
 
 //todo: make the divs draggable
+//todo: when you double click, it should open a write menu
