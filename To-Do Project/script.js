f468d8f73f0c145da6e69d56a8449b19990e0959
Ebert Soares <59069184+Ebert-Soares@users.noreply.github.com>
2021-03-01 09:03:58 -0300

    I fucking hate CSS

--- To-Do Project/script.js
+++ To-Do Project/script.js
@@ -1,11 +1,18 @@
 
 var task;
+var retangleNumber = 0;
+
 
 function task(){
+retangleNumber+=1;
 var task = prompt("Digite a sua task: ");
 
 
+document.getElementById('retangle' + retangleNumber).style.height =  + Math.round(task.length/12) + 'rem';
+document.getElementById('retangle' + retangleNumber).textContent = task;
+
 }
+
 //todo: create the "turn white" system
 //todo: make the getelement and change the text inside it
 //todo: make the divs draggable
