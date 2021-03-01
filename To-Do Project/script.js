b3bb17f426288c79e4fadb0c8afb8f1aff657b58
Ebert Soares <59069184+Ebert-Soares@users.noreply.github.com>
2021-03-01 20:02:13 -0300

    É TETRA É TETRA! FInally could fix the bug and double click is working

--- To-Do Project/script.js
+++ To-Do Project/script.js
@@ -25,31 +25,59 @@ else if(task.length<20 && task.length>0){
 else{
   alert('More than 60 characters');
 }
+
+//doubleclick callings
+document.getElementById("retangle1").addEventListener("dblclick", function(){
+  doubleclick(1);
+}, false);
+
+document.getElementById("retangle2").addEventListener("dblclick", function(){
+  doubleclick(2);
+});
+
+document.getElementById("retangle3").addEventListener("dblclick", function(){
+  doubleclick(3);
+});
+
+document.getElementById("retangle4").addEventListener("dblclick", function(){
+  doubleclick(4);
+});
+
+document.getElementById("retangle5").addEventListener("dblclick", function(){
+  doubleclick(5);
+});
+
+document.getElementById("retangle6").addEventListener("dblclick", function(){
+  doubleclick(6);
+});
+
+
 }
+
 
 
 function doubleclick(rewrite){
 
 var newtask = prompt("Digite a sua task: (maximum of 60 characters)");
 
+
 if(newtask.length<60 && newtask.length>20){
+
 document.getElementById('retangle' + rewrite).style.height =  1 + Math.round(newtask.length/20) + 'rem';
 document.getElementById('retangle' + rewrite).textContent = ' ' + newtask ;}
 
-else if(task.length<20 && task.length>0){
+else if(newtask.length<20 && newtask.length>0){
 
-    document.getElementById('retangle' + retangleNumber).style.height =   2 +'rem';
-    document.getElementById('retangle' + retangleNumber).textContent = ' ' + task;
+    document.getElementById('retangle' + rewrite).style.height =   2 +'rem';
+    document.getElementById('retangle' + rewrite).textContent = ' ' + newtask;
 
 }}
 
 
-document.getElementById("retangle1").addEventListener("dblclick", doubleclick(rewrite=1));
-document.getElementById("retangle2").addEventListener("dblclick", doubleclick(rewrite=2));
-document.getElementById("retangle3").addEventListener("dblclick", doubleclick(rewrite=3));
-document.getElementById("retangle1").addEventListener("dblclick", doubleclick(rewrite=4));
+
 
 
 
 //todo: make the divs draggable
 //todo: when you double click, it should open a write menu
+//todo: add good quotes when the user drops the div on the coloured square
