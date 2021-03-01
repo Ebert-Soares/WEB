f14465daae2d187d407d09b6003d5e1305874865
Ebert Soares <59069184+Ebert-Soares@users.noreply.github.com>
2021-03-01 12:11:45 -0300

    Added text align and border

--- To-Do Project/script.js
+++ To-Do Project/script.js
@@ -9,13 +9,16 @@ var task = prompt("Digite a sua task: (maximum of 36 characters)");
 
 if(task.length<36 && task.length>12){
 document.getElementById('retangle' + retangleNumber).style.backgroundColor = 'white';
+document.getElementById('retangle' + retangleNumber).style.border = '2px solid grey';
 document.getElementById('retangle' + retangleNumber).style.height =  1 + Math.round(task.length/12) + 'rem';
-document.getElementById('retangle' + retangleNumber).textContent = task;}
+document.getElementById('retangle' + retangleNumber).textContent = ' ' + task + ' ';}
 
 else if(task.length<12 && task.length>0){
     document.getElementById('retangle' + retangleNumber).style.backgroundColor = 'white';
     document.getElementById('retangle' + retangleNumber).style.height =   2 +'rem';
-    document.getElementById('retangle' + retangleNumber).textContent = task;
+    document.getElementById('retangle' + retangleNumber).textContent = ' ' + task + ' ';
+    document.getElementById('retangle' + retangleNumber).style.border = '2px solid grey';
+
 }
 
 else{
