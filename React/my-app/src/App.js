ac31268b5f50d0aa17e7c52b150f6028d2896f43
Ebert Soares <59069184+Ebert-Soares@users.noreply.github.com>
2021-03-09 14:01:28 -0300

    Better organized.

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -28,10 +28,10 @@ function App() {
 {list.map(function(item) {
 return(  <div>
   <span>
-  <a href={item.url}>{item.title}</a>
+  <a href={item.url}>{item.title + ' '}</a>
   </span>
-  <span>{item.author}</span>
-  <span>{item.num_comments}</span>
+  <span>{item.author + ' '}</span>
+  <span>{item.num_comments + ' '}</span>
   <span>{item.points}</span>
   </div>
   );
