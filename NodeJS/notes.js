b243f54c3b4828b993a3fa339a86487de97e9d64
Ebert Soares <59069184+Ebert-Soares@users.noreply.github.com>
2021-04-01 14:44:05 -0300

    error

--- NodeJS/notes.js
+++ NodeJS/notes.js
@@ -5,7 +5,17 @@ const fs = require('fs');
 
 
 
+function loadNotes(){
 
+try{
+  const buffer = fs.readFileSync("notes.json");
+  const JSON1 = buffer.toString();
+  return JSON.parse(JSON1);
+}catch (e){
+  return[]
+}
+
+}
 
 
 
@@ -19,8 +29,18 @@ const getNotes = ()=>{
 
 
 
-
 const addNote = (title, body)=>{
+loadNotes();
+  const loadNotes = loadNotes();
+  loadNotes.push({
+    title: title,
+    body: body,
+  })
+  saveNotes(loadNotes);
+  const saveNotes = loadNotes=>{
+    const dJSON = JSON.stringify(loadNotes);
+    fs.writeFileSync("notes.json", loadNotes)
+  }
 
 }
 
