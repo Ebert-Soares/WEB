9fd73c34acac45483f774fa4193d45331f26e8e5
Ebert Soares <59069184+Ebert-Soares@users.noreply.github.com>
2021-04-02 14:27:39 -0300

    Need to write on a file

--- NodeJS/Weather/app.js
+++ NodeJS/Weather/app.js
@@ -1,1 +1,15 @@
 const request = require('request');
+const fs = require('fs');
+
+
+const url = "http://apiadvisor.climatempo.com.br/api/v1/forecast/locale/8654/hours/72?token=ca2f7c5bf97405e14cbff5bef581086e"
+
+
+
+
+
+request({url: url}, (error, response) => {
+  const data = JSON.parse(response.body);
+  fs.writeFileSync("data.txt", data);
+}
+)
