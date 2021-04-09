ffcce947f968a3df03f0d80c9940b6a6d7dde520
Ebert Soares <59069184+Ebert-Soares@users.noreply.github.com>
2021-04-09 15:28:03 -0300

    a

--- NodeJS/web-server/src/app.js
+++ NodeJS/web-server/src/app.js
@@ -1,7 +1,12 @@
+const path = require('path')
 const express = require("express");
 const app = express()
+
+
+app.use(express.static(path.join(__dirname, "../public")))
+
 app.get('', (req, res)=>{
-  res.send("<h1>Weather</h1>");
+  res.send(path.join());
 });
 
 
