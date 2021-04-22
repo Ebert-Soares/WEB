60d9faacc83eb5687eb6799bc734d4574372d01d
Ebert Soares <59069184+Ebert-Soares@users.noreply.github.com>
2021-04-22 15:37:45 -0300

    green square

---
+++ Three.js/test.js
@@ -0,0 +1,31 @@
+
+
+function main(){
+const canvas = document.querySelector("#c");
+
+const renderer = new THREE.WebGLRenderer({canvas});
+
+const fov = 75;
+const aspect = 2;
+const near = 0.1;
+const far = 5;
+
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 2;
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+const material = new THREE.MeshBasicMaterial({color: 0x44aa88});
+
+const cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
+
+renderer.render(scene, camera);
+
+
+}
+
+main();
